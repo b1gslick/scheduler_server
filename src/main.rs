c8c383f8a0374a45f37ba07baefc94400b3ccc5c
b1gslick <bigslick1106@gmai.com>
2024-01-29 09:21:04 +0200

    add e2e to github actions

--- src/main.rs
+++ src/main.rs
@@ -147,5 +147,5 @@ async fn main() {
         .with(warp::trace::request())
         .recover(return_error);
 
-    warp::serve(routes).run(([127, 0, 0, 1], config.port)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], config.port)).await;
 }
